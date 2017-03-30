fe16f013efabc8eea427b7bc06b84138d138d2f4
Allen <scott.allen@capgemini.com>
2017-03-30 09:41:08 +0100

    Initial Public Talk API Integration without tests

---
+++ src/API/talk.js
@@ -0,0 +1,57 @@
+const endpoint = "http://cfp.devoxx.co.uk/api/conferences/DV17/talks/";
+
+/**
+ * Get a talk by it's Id
+ * @param talkId
+ * @returns (Object) {id, name, description, tracks[track], speakers[{id, name}])
+ */
+let getTalk = (talkId) => {
+    fetch(endpoint + talkId, {
+        method: 'GET'
+    }).then((response) => {
+        let body = JSON.parse(response.body);
+
+        return {
+            id: body.id,
+            name: body.title,
+            description: body.summary,
+            tracks: parseTracks(body.track),
+            speakers: parseSpeakers(body.speakers)
+        };
+
+    }).catch((error) => {
+        return {error: error};
+    })
+};
+
+/**
+ * Takes a comma delimited string and turns it into an array of individual "tracks"
+ * @param tracks
+ * @returns {Array} tracks
+ */
+let parseTracks = (tracks) => {
+    return tracks.split(",").map((item) => {return item.trim()});
+};
+
+/**
+ * Takes the speaker object returned and formats it to the way we'd like it
+ * @param speakers
+ * @returns {Array} speakers
+ */
+let parseSpeakers = (speakers) -> {
+    return speakers.map((speakerObj) => {
+        return {
+            id: getSpeakerIdFromUrl(speakerObj.link.href),
+            name: speakerObj.name
+        };
+    });
+};
+
+/**
+ * Take the url from a speaker and return the id
+ * @param url
+ * @returns {string} id
+ */
+let getSpeakerIdFromUrl = (url) => {
+    return url.slice(url.lastIndexOf("/") + 1).trim();
+};
