4c3987b1ed2c74345d84ffc5a356be5225203c45
Eade <sam.eade@capgemini.com>
2017-03-30 12:10:27 +0100

    included initial implementation of speaker API

---
+++ src/API/speaker.js
@@ -0,0 +1,47 @@
+const speakerEndpoint = "http://cfp.devoxx.co.uk/api/conferences/DV17/speakers/";
+
+/**
+ * Get a speaker by Id
+ *
+ * @param speakerId
+ * @returns {bio, firstName, lastName, avatarUrl, company, twitter, blog,
+ *          acceptedTalks[acceptedTalks], links, talkType, track[track], talkId, talkTitle}
+ */
+let getSpeaker = (speakerId) => {
+    fetch(speakerEndpoint + speakerId, {
+        method: 'GET'
+    }).then((response) => {
+        let body = JSON.parse(response.body);
+
+        return {
+            //speaker object
+            bio: body.bio,
+            firstName: body.firstName,
+            lastName: body.lastName,
+            avatarUrl: body.avatarUrl,
+            company: body.company,
+            twitter: body.twitter,
+            blog: body.blog,
+            acceptedTalks: body.acceptedTalks,
+            links: body.acceptedTalks.links,
+            talkType: body.talkType,
+            track: parseTrack(body.acceptedTalks.track),
+            talkId: body.talkId,
+            talkTitle: body.talkTitle
+        };
+
+    }).catch((error) => {
+        return {error: error};
+    })
+};
+
+/**
+ * Creates track array from String of tracks
+ *
+ * @param track
+ * @returns {Array}
+ */
+let parseTrack = (track) => {
+    return track.split(",").map((item) => {
+        return item.trim()});
+}
