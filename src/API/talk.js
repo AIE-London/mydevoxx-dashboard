9f85ad6092a3570926adb7a78e25a2c18b4a6f7b
Allen <scott.allen@capgemini.com>
2017-03-30 16:42:05 +0100

    Added in jest tests, replaced fetch with then-request

--- src/API/talk.js
+++ src/API/talk.js
@@ -1,11 +1,13 @@
+import request from 'then-request';
+
 let endpoint = "http://cfp.devoxx.co.uk/api/conferences/DV17/talks/";
+const mockEndpoint = "https://aston-wiremock.eu-gb.mybluemix.net/api/conferences/DV17/talks/"; // Expect JWG-0522 as talkId
 
 /**
- * Use a mocked endpoint
+ * Use mock endpoint when not Live
  */
-if (process.env.NODE_ENV != "production"){
-    console.log("Not in production, using mocked endpoint");
-    endpoint = "https://aston-wiremock.eu-gb.mybluemix.net/api/conferences/DV17/talks/"; // Only works with talkId JWG-0522
+if ( ["production", "integration"].indexOf(process.env.NODE_ENV) < 0){
+    endpoint = mockEndpoint;
 }
 
 /**
@@ -14,11 +16,9 @@ if (process.env.NODE_ENV != "production"){
  * @returns (Object) {id, name, description, tracks[track], speakers[{id, name}])
  */
 let getTalk = (talkId) => {
-    fetch(endpoint + talkId, {
-        method: 'GET'
-    }).then((response) => {
-        let body = JSON.parse(response.body);
-
+    /*endpoint + talkId*/
+    return request('GET', endpoint + talkId).then((response) => {
+        let body = JSON.parse(response.getBody());
         return {
             id: body.id,
             name: body.title,
@@ -26,19 +26,21 @@ let getTalk = (talkId) => {
             tracks: parseTracks(body.track),
             speakers: parseSpeakers(body.speakers)
         };
-
-    }).catch((error) => {
-        return {error: error};
-    })
+    });
 };
 
 /**
  * Takes a comma delimited string and turns it into an array of individual "tracks"
+ * If no tracks are provided - we return an empty array.
  * @param tracks
  * @returns {Array} tracks
  */
 let parseTracks = (tracks) => {
-    return tracks.split(",").map((item) => {return item.trim()});
+    let result = [];
+    if (tracks) {
+        result = tracks.split(",").map((item) => {return item.trim()});
+    }
+    return result;
 };
 
 /**
@@ -46,7 +48,7 @@ let parseTracks = (tracks) => {
  * @param speakers
  * @returns {Array} speakers
  */
-let parseSpeakers = (speakers) -> {
+let parseSpeakers = (speakers) => {
     return speakers.map((speakerObj) => {
         return {
             id: getSpeakerIdFromUrl(speakerObj.link.href),
@@ -56,14 +58,35 @@ let parseSpeakers = (speakers) -> {
 };
 
 /**
+ * InvalidURLException
+ * For when a invalid URL is given
+ */
+function InvalidURLException (message) {
+    this.name = 'InvalidURLException';
+    this.message = message || 'Invalid URL Provided';
+};
+InvalidURLException.prototype = Error.prototype;
+
+/**
  * Take the url from a speaker and return the id
  * @param url
  * @returns {string} id
+ * @throws InvalidURLException
  */
 let getSpeakerIdFromUrl = (url) => {
-    return url.slice(url.lastIndexOf("/") + 1).trim();
+    let result = "";
+
+    if (url){
+        result = url.slice(url.lastIndexOf("/") + 1).trim();
+    } else {
+        throw new InvalidURLException();
+    }
+    return result;
 };
 
-module.exports = {
-    getTalk: getTalk
+export default {
+    getTalk: getTalk,
+    parseTracks: parseTracks,
+    parseSpeakers: parseSpeakers,
+    getSpeakerIdFromUrl: getSpeakerIdFromUrl
 };
