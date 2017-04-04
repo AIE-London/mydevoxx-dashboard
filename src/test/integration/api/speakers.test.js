6a6638e17ec0d34825c43cf09ae8a8f29619627d
Allen <scott.allen@capgemini.com>
2017-04-04 15:12:34 +0100

    added negative tests and post wiremock mapping on run

--- src/test/integration/api/speakers.test.js
+++ src/test/integration/api/speakers.test.js
@@ -1,23 +1,39 @@
-/**
- * Created by SCMORETO on 30/03/2017.
- */
-
 import speakers from "../../../main/api/speakers";
+import wireMockAPI from "./wiremock/wiremock-api";
+import speakersMapping from "./wiremock/mappings/speakers";
+import notFound from "./wiremock/mappings/404";
 
+let normalSetup = () => {
+    return wireMockAPI.postMapping(speakersMapping);
+};
+
+let notFoundSetup = () => {
+    return wireMockAPI.postMapping(notFound);
+};
 
 describe('getSpeakers', () => {
-
-    it('should return a re-formatted array of speakers names and UUID', () => {
-        return speakers.getSpeakers().then(function (speakers) {
-
-            expect(speakers).toEqual([{
-                id: '695b40d928dd0a905b7ab1b900b5a5752870a7d8',
-                name: 'Helen Beal'
-            }])
-
-        })
-
-
-    });
-
+    it('should work return data and handles a 404', () => {
+        return normalSetup() // Setup mock for positive test
+            .then(speakers.getSpeakers)
+            .then((results) => {
+                expect(results[0]).toEqual({
+                    id: '695b40d928dd0a905b7ab1b900b5a5752870a7d8',
+                    name: 'Helen Beal'
+                })
+            }, (error) => { // Should not error, so thro expect error below
+                expect(true).toBe(false); // Throw an expect error to differentiate from 404 in the catch
+            })
+            .then(notFoundSetup) // Setup Mock for 404 response
+            .then(speakers.getSpeakers)
+            .then((results) => {
+                expect(true).toBe(false); // Should not get here, so fair with expect error
+            })
+            .catch((error) => {
+                if (error.statusCode){ // Failed on negative test - good!
+                    expect(error.statusCode).toBe(404);
+                } else { // Failed on positive test - bad!
+                    expect(true).toBe(false);
+                }
+            })
+    })
 });
