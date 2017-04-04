01dbbf9fcf70826eb6f19e541edb0210b7f819d6
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-04-04 10:56:51 +0100

    added initial component

--- src/main/App.js
+++ src/main/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TalkCard from './components/TalkCard/';
 import './App.css';
+import {Col, Row} from 'react-flexbox-grid';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Row center="xs">
+          <Col xs={10}>
+            <TalkCard></TalkCard>
+          </Col>
+        </Row>
       </div>
     );
   }
