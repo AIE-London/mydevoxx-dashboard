7bde38404f01c260e6c8e028014eff86c7aab32b
DanielLindsay <dl.12321.dl@googlemail.com>
2017-04-05 11:35:37 +0100

    Component is fully formatted

--- src/main/components/SpeakerCard/index.js
+++ src/main/components/SpeakerCard/index.js
@@ -2,40 +2,42 @@
  * Created by DLINDSAY on 03-Apr-17.
  */
 import React from 'react';
-//import StyleSheet from 'react-style';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Title from '../SpeakerTitle';
 import Card from '../Card';
+import Default from './defaultIcon.png';
+import styled from 'styled-components';
 
+const ImgCircle = styled.img`
+    borderRadius: 50%;
+    width: 50px;
+    height: 50px;
+    marginTop: 20px;
+    border: 5px solid white;
+`;
 
-const circle = {
-        borderRadius: 50,
-        width: 50,
-        height: 50,
-        background: 'green'
-};
-
-const flexContainer = {
-        flex: 1
-
-};
+const Blog = styled.div`
+    paddingLeft: 30px;
+    paddingBottom: 10px,;
+    paddingTop: 2px;
+`;
 
 class speakerCard extends React.Component {
 
     render() {
         return <Card><Grid fluid>
             <Row>
-                <Col xs={6} md={2} >
-                    <Row around="xs" middle="xs">
-                        <div style={circle}></div>
-                        <Title name={this.props.name} company={this.props.company}/>
-                    </Row>
+                <Col>
+                    <ImgCircle src={Default} ></ImgCircle>
+                </Col>
+                <Col xs>
+                    <Title name={this.props.name} company={this.props.company}/>
                 </Col>
             </Row>
             <Row>
                 <Col md={3}>
                     <div>Personal Blog:</div>
-                    <div style={{paddingLeft: 30, paddingBottom: 10, paddingTop: 2}}>{this.props.blog}</div>
+                    <Blog>{this.props.blog}</Blog>
                 </Col>
             </Row>
             <Row>
@@ -56,23 +58,6 @@ class speakerCard extends React.Component {
 
     }
 }
-/*
-class speakerCard extends React.Component {
-    render() {
-        return <div style={flexContainer}>
-            <div style={circle}></div>
-            <div>name goes here</div>
-            <div>title goes here and there</div>
-            <ul>
-                <li>info 1 here</li>
-                <li>info 2 here</li>
-                <li>info 3 here</li>
-            </ul>
-        </div>;
-
-    }
-}
-*/
 
 
 export default speakerCard;
