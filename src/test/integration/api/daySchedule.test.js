91b77f2859aa5bb7d095de628f663160d42026e5
Moreton <scott.moreton@capgemini.com>
2017-04-05 15:38:14 +0100

    daySchedule test initial impl

--- src/test/integration/api/daySchedule.test.js
+++ src/test/integration/api/daySchedule.test.js
@@ -1,13 +1,18 @@
-import speakers from "../../../main/api/speakers";
+/**
+ * Created by SCMORETO on 05/04/2017.
+ */
+
+import daySchedule from "../../../main/api/daySchedule";
 import wireMockAPI from "./wiremock/wiremock-api";
-import speakersMapping from "./wiremock/mappings/speakers";
-import notFound from "./wiremock/mappings/notFoundSpeakers";
+import dayScheduleMapping from "./wiremock/mappings/daySchedule";
+import notFound from "./wiremock/mappings/notFoundDaySchedule";
+import emptyArray from "./wiremock/mappings/emptyArrayDaySchedule";
 
 /**
  * Set up wiremock with normal room api response
  */
 let normalSetup = () => {
-    return wireMockAPI.postMapping(speakersMapping);
+    return wireMockAPI.postMapping(dayScheduleMapping);
 };
 
 /**
@@ -17,24 +22,37 @@ let notFoundSetup = () => {
     return wireMockAPI.postMapping(notFound);
 };
 
-/**a
- * <if> wiremock is trained with a speakers response list -> returns speaker list
- * <else> wiremock is trained with a 404 response -> returns 404
+/**
+ * Set up wiremock with empty Array response
  */
-describe('getSpeakers', () => {
-    it('should work return data and handles a 404', () => {
+let emptyArraySetup = () => {
+    return wireMockAPI.postMapping(emptyArray);
+};
+
+
+
+/**
+ * <if> wiremock is trained with a daySched response list -> returns a day schedule array list
+ * <else> wiremock is trained with a 404 response -> returns 404
+ * <else-if> wiremock qill return an empty slots array
+ */
+describe('getSlots', () => {
+    it('should work return data and handles a 404 or empty array', () => {
         return normalSetup() // Setup mock for positive test
-            .then(speakers.getSpeakers)
+            .then(daySchedule.getSlots("thursday"))
             .then((results) => {
                 expect(results[0]).toEqual({
-                    id: '695b40d928dd0a905b7ab1b900b5a5752870a7d8',
-                    name: 'Helen Beal'
+                    roomId: 'a_gallery_hall',
+                    fromTimeMillis: 1494493200000,
+                    talkId: 'JWG-0522',
+                    toTimeMillis: 1494494400000,
+                    roomName: 'Gallery Hall'
                 })
             }, (error) => { // Should not error, so thro expect error below
                 expect(true).toBe(false); // Throw an expect error to differentiate from 404 in the catch
             })
             .then(notFoundSetup) // Setup Mock for 404 response
-            .then(speakers.getSpeakers)
+            .then(daySchedule.getSlots("thursday"))
             .then((results) => {
                 expect(true).toBe(false); // Should not get here, so fair with expect error
             })
