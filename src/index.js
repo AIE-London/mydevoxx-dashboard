4ab8a0eaf5e626313587327da342f6b6302f5e70
DanielLindsay <dl.12321.dl@googlemail.com>
2017-04-05 14:05:55 +0100

    Removed properties from App in Index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './main/components/SpeakerCard.js';
+import App from './main/App';
 
 import './index.css';
 
 ReactDOM.render(
-  <App name="Testy MacTest" company="Testy Inc." blog="forsomereasonihaveablog.com"
-       talks={["The Importance of Jazz in modern society","Rory's Roaring Apps: An M.Night Shayamalan production","That other talk"]}/>,
+  <App />,
   document.getElementById('root')
 );
