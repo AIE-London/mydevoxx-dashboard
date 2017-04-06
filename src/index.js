ba0bf882722667e769bba5250eafb520b9919fc7
DanielLindsay <dl.12321.dl@googlemail.com>
2017-04-06 10:02:33 +0100

    Stats component now uses properties

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './main/App';
+import App from './main/components/Stats';
 
 import './index.css';
 
 ReactDOM.render(
-  <App/>,
+  <App tracks={["Agile", "Bgile", "Cgile"]} speakers={["Person One", "Person Two", "Person Three"]}/>,
   document.getElementById('root')
 );
---
+++ src/main/components/Stats.js
@@ -0,0 +1,64 @@
+/**
+ * Created by DLINDSAY on 05-Apr-17.
+ */
+import React from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import Card from './Card';
+import styled from 'styled-components';
+import {CommaList, CommaListItem} from './CommaList';
+
+
+const Container = styled.div`
+    width: 400px;
+    height: 400px;
+`;
+
+const StatHeader = styled.h2`
+    
+    font-family: helvetica;
+`;
+
+class Stats extends React.Component {
+    render() {
+        return <Container><Card center="xs">
+            <Col xs={10}>
+                <Col xs={6}>
+                    <Row start="xs">
+                        <h1>My Stats</h1>
+                    </Row>
+                </Col>
+
+                <Row start="xs">
+                    <Col xs={10}>
+
+                        <StatHeader>Top Tracks:</StatHeader>
+
+                        <CommaList>
+                            {
+                                this.props.tracks.map( function(track) {
+                                    return <CommaListItem key={track}>{track}</CommaListItem>
+                                })
+                            }
+
+                        </CommaList>
+
+                        <StatHeader>Top Speakers:</StatHeader>
+
+                        <CommaList>
+                            {
+                                this.props.speakers.map( function(speaker) {
+                                    return <CommaListItem key={speaker}>{speaker}</CommaListItem>
+                                })
+                            }
+
+                        </CommaList>
+
+                    </Col>
+                </Row>
+            </Col>
+
+        </Card></Container>
+    }
+}
+
+export default Stats;
