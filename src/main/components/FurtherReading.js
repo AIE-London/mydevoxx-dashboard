348f184fd6ae6c64636f30ace60f5438e616a6ba
Sadler <tom.sadler@capgemini.com>
2017-04-07 13:41:36 +0100

    made into card

--- src/main/components/FurtherReading.js
+++ src/main/components/FurtherReading.js
@@ -6,35 +6,41 @@ import {Grid, Row, Col} from 'react-flexbox-grid';
 import styled from 'styled-components';
 import Card from './Card';
 
+const Container  = styled.div`
+  width: 400px;
+  height: 400px;
+`;
 
 const StatMainHeader = styled.h1`
     text-align: left;
-    font-size: 2.5em;
+    font-size: 2em;
     font-family: helvetica;
+    margin-left: 15px;
 `;
 
 const StatValue = styled.p`
     text-align: center;
-    font-size: 1.5em;
+    font-size: 1em;
     font-family: helvetica;
 `;
 
 const LinkValue = styled.p`
-    text-align: center;
-    font-size: 1em;
+    font-size: 0.75em;
     color: #fff;
     font-family: helvetica;
+     margin-left: 15px;
 `;
 
 class furtherReading extends React.Component {
     render() {
 
-        return <Card>
+        return <Container>
+        <Card>
             <Row center="xs">
                     <Col xs={12}>
 
 
-                        <Col xs={2}>
+                        <Col xs={5}>
                             <Row start="xs">
                                 <StatMainHeader>AdvancedReading</StatMainHeader>
                                 <LinkValue> <a href> http:www.bbc.com </a> </LinkValue>
@@ -45,8 +51,7 @@ class furtherReading extends React.Component {
                             <Col xs={10}>
 
                                 <StatValue>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at gravida neque. Suspendisse
-                                    vitae lacus eget tellus facilisis finibus. Integer vestibulum, risus vel molestie ornare,
-                                    urna lacus posuere libero, in convallis ipsum enim at eros.... </StatValue>
+                                    vitae lacus eget tellus facilisis .... </StatValue>
 
                             </Col>
                         </Row>
@@ -54,6 +59,7 @@ class furtherReading extends React.Component {
 
                 </Row>
             </Card>
+        </Container>
 
     }
 }
