4220245934b1f21b4b5187c66481f4a2f4ed680d
Shefik <caner.shefik@capgemini.com>
2017-04-07 14:38:39 +0100

    Tests pass, session view

--- src/main/App.js
+++ src/main/App.js
@@ -4,13 +4,6 @@ import './App.css';
 import {Col, Row} from 'react-flexbox-grid';
 import ReportStats from './components/ReportStats';
 
-let reportStatsData =
-    {
-        "minutes": 455,
-        "talks": 10,
-        "learning": "JS, Polymer, Java" ,
-        "attendees": 435
-    };
 
 
 import SessionView from './components/SessionView';
