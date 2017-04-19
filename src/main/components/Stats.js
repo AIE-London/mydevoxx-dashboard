235fe32cd8e8c8f20936efe20a0019ead9f9d3be
Moreton <scott.moreton@capgemini.com>
2017-04-19 10:09:45 +0100

    Removed some commenting from Stats.js

--- src/main/components/Stats.js
+++ src/main/components/Stats.js
@@ -20,34 +20,33 @@ const StatHeader = styled.h2`
 class Stats extends React.Component {
     render() {
         return <Container><Card center="xs"> {/* Wraps the Card component within the Container component */}
-            <Col xs={10}> {/* creates a column that is 10 out of 12 sections wide */}
-                <Col xs={6}> {/* creates a column that is 6 out of 12 sections wide */}
-                    <Row start="xs"> {/* creates a row that aligns the object within it to the left */}
+            <Col xs={10}>
+                <Col xs={6}>
+                    <Row start="xs">
                         <h1>My Stats</h1>
                     </Row>
                 </Col>
 
-                <Row start="xs"> {/* creates a row that aligns the object within it to the left */}
-                    <Col xs={12}> {/* creates a column that is 12 out of 12 sections  */}
-
-                        <StatHeader>Top Tracks:</StatHeader> {/* creates a header using the StatHeader styled h2 format */}
-                        <div style={{textAlign: 'center'}}> {/* creates a div which aligns the text within it to the centre */}
+                <Row start="xs">
+                    <Col xs={12}>
+                        <StatHeader>Top Tracks:</StatHeader>
+                        <div style={{textAlign: 'center'}}>
                             <CommaList > {/* creates a ul element using the CommaList styled ul format */}
                                 {
-                                    this.props.tracks.map( function(track) { //returns a styled li element for each of the items in the tracks property array
+                                    this.props.tracks.map( function(track){
                                         return <CommaListItem key={track}>{track}</CommaListItem>
                                     })
                                 }
                             </CommaList>
                         </div>
 
-                        <StatHeader>Top Speakers:</StatHeader> {/* creates a header using the StatHeader styled h2 format */}
+                        <StatHeader>Top Speakers:</StatHeader>
 
-                        <div style={{textAlign: 'center', paddingBottom: 15}}> {/* creates a div that aligns all text within it to the centre and adds 15 pixels of padding to the bottom */}
-                            <CommaList> {/* creates a ul element using the CommaList styled ul format */}
+                        <div style={{textAlign: 'center', paddingBottom: 15}}>
+                            <CommaList>
                                 {
                                     this.props.speakers.map( function(speaker) {
-                                        return <CommaListItem key={speaker}>{speaker}</CommaListItem> //returns a styled li element for each of the items in the tracks property array
+                                        return <CommaListItem key={speaker}>{speaker}</CommaListItem>
                                     } )
                                 }
                             </CommaList>
