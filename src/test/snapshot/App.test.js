65849560b8fcd35acb22e18f86ae8a1516bf9aa1
Eade <sam.eade@capgemini.com>
2017-04-20 16:08:35 +0100

    app.js test changes

--- src/test/snapshot/App.test.js
+++ src/test/snapshot/App.test.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import App from '../../main/App';
 import renderer from 'react-test-renderer';
 
+import testImage from './images/test-image.jpeg';
+
+const talkDetail =
+    {
+        dayNo: 'One',
+        sTime: '10:00',
+        room: 'Mezzanine',
+        title: 'Welcome to Devoxx 2017',
+        description: 'Join the organisers of Devoxx UK and great keynote ' +
+        'speakers for inspring stories in 20 minute segments.',
+        rating: 4,
+        topTracks: [
+            'Java',
+            'Devoxx',
+            'Spring',
+        ],
+        notes: 'Lorem ipsum dolor sit amet, everti quaestio mel ea. Ex eos ' +
+        'volutpat qualisque. Sale tantas cotidieque quo ut, ad nostro consectetuer' +
+        ' nec. Feugiat qualisque quo an. Labores officiis te nam.',
+        review: {
+            name: 'Test User',
+            comment: 'Great session, thanks for organising. Looking forward to the next one!',
+            image: testImage
+        },
+        speakers: [
+            {
+                name: 'Test Speaker',
+                company: 'Capgemini',
+                blog: 'personalblog.com',
+                talks: [
+                    'Intro to Devoxx (Room 1 - 11:45)',
+                    'Intro to Devoxx 2 (Room 2 - 13:45)'
+                ]
+            }
+        ]
+    };
 
 let reportStatsData =
     {
@@ -25,7 +61,7 @@ test('app component', () => {
 
 test('App component snapshot', () => {
     const tree = renderer.create(
-      <App reportStats={reportStatsData}/>
+      <App reportStats={reportStatsData}  talk={talkDetail}/>
     ).toJSON();
     expect(tree).toMatchSnapshot();
 });
