f296aebdb4c134d8c8c7d59de4f82e1ced2750fd
Eade <sam.eade@capgemini.com>
2017-04-26 16:54:36 +0100

    included UserEmail cpmponent tests

--- src/test/snapshot/components/UserEmail/UserEmail.test.js
+++ src/test/snapshot/components/UserEmail/UserEmail.test.js
@@ -1,1 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import UserEmail from "../../../../main/components/UserEmail";
+
+test("user email component", () => {
+    it("will render without crashing", () => {
+        const div = document.creatElement("div");
+        ReactDOM.render(<UserEmail/>, div);
+    });
+});
+
+test("user email component with snapshot", () => {
+    const tree = renderer.create(<UserEmail/>).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
 
