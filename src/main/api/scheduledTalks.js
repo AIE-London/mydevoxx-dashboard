320a1336def5243b3484ff67a040d7a15ccec7fc
Moreton <scott.moreton@capgemini.com>
2017-04-27 10:32:32 +0100

    Created the api call for scheduled talks, tested using wiremock

---
+++ src/main/api/scheduledTalks.js
@@ -0,0 +1,34 @@
+/**
+ * Created by SCMORETO on 26/04/2017.
+ */
+
+/**
+ * Created by SCMORETO on 19/04/2017.
+ */
+import request from 'then-request';
+
+let ScheduledTalksEndpoint = "";
+const mockUuidEndpoint = "https://aston-wiremock.eu-gb.mybluemix.net/";
+
+/**
+ * Use mock endpoint outside of live
+ */
+if (["production", "integration"].indexOf(process.env.NODE_ENV) < 0) {
+    ScheduledTalksEndpoint = mockUuidEndpoint;
+}
+
+/**
+ * Get the users Scheduled Talks for the conference
+ * @Params uuid
+ * @returns {UUID}
+ */
+let getScheduledTalks = (uuid) => {
+    return request('GET', ScheduledTalksEndpoint + uuid + '/scheduled').then((response) => {
+        let body = response.getBody();
+        return body.toString();
+    });
+};
+
+export default {
+    getScheduledTalks
+};
