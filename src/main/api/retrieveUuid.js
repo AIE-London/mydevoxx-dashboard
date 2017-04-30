8c26cf8fca75309d1198a51b35c99ad377cee857
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-04-30 20:26:03 +0100

    updated endpoint + fixed styling bug

--- src/main/api/retrieveUuid.js
+++ src/main/api/retrieveUuid.js
@@ -1,28 +1,30 @@
-import request from 'then-request';
+import request from "then-request";
 
-let UuidEndpoint = "http://cfp.devoxx.co.uk/uuid?email=";
-const mockUuidEndpoint = "https://aston-wiremock.eu-gb.mybluemix.net/uuid?email=";
+let UuidEndpoint =
+  "http://mydevoxx-capgemini-api-router.eu-gb.mybluemix.net/uuid?email=";
+const mockUuidEndpoint =
+  "https://aston-wiremock.eu-gb.mybluemix.net/uuid?email=";
 
 /**
  * Use mock endpoint outside of live
  */
 if (["production", "integration"].indexOf(process.env.NODE_ENV) < 0) {
-    UuidEndpoint = mockUuidEndpoint;
+  UuidEndpoint = mockUuidEndpoint;
 }
 
 /**
  * Get the users UUID  for the conference
  * @returns {UUID}
  */
-let getUUID = (email) => {
-    return request('GET', UuidEndpoint + email).then((response) => {
-        let body = response.getBody();
+let getUUID = email => {
+  return request("GET", UuidEndpoint + email).then(response => {
+    let body = response.getBody();
 
-       // return body;
-        return body.toString();
-    });
+    // return body;
+    return body.toString();
+  });
 };
 
 export default {
-    getUUID
+  getUUID
 };
