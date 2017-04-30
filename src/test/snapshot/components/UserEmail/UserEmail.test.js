287018ddcb35003f46d8c1671fe944c6e9edd4a9
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-04-30 01:23:50 +0100

    test updates

--- src/test/snapshot/components/UserEmail/UserEmail.test.js
+++ src/test/snapshot/components/UserEmail/UserEmail.test.js
@@ -3,16 +3,27 @@ import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
 import UserEmail from "../../../../main/components/UserEmail";
 
+/*
+    Added function to add required functions onto mock DOM node.
+    This is necessary to support 'ref' usage in tests.
+
+    By default - Jest will not allow ref usage.
+    Issue: https://github.com/facebook/react/issues/7740
+ */
+function createNodeMock() {
+  return {
+    focus() {}
+  };
+}
+
 test("user email component", () => {
-    it("will render without crashing", () => {
-        const div = document.creatElement("div");
-        ReactDOM.render(<UserEmail/>, div);
-    });
+  it("will render without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<UserEmail />, div);
+  });
 });
 
 test("user email component with snapshot", () => {
-    const tree = renderer.create(<UserEmail/>).toJSON();
-    expect(tree).toMatchSnapshot();
+  const tree = renderer.create(<UserEmail />, { createNodeMock }).toJSON();
+  expect(tree).toMatchSnapshot();
 });
-
-
