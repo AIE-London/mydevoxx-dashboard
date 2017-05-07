d3954ec0ca4607a8b606cd4e0c6ba33515b91887
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-05-07 13:30:26 +0100

    limit of 3 on speaker recommendations

--- src/main/utils/recommendationEngine.js
+++ src/main/utils/recommendationEngine.js
@@ -11,17 +11,34 @@ import { getTopTracks } from "./talkUtils";
 
 export function recommendGlobal(talks, speakers) {
   let topTracks = getTopTracks(talks);
+  let speakerLimit = 3;
+
   /*
     Recommend based on speakers
    */
-  let speakerRecommendations = speakers.map(speaker => {
-    return new Recommendation(
-      speaker.speaker.name + "'s Blog",
-      speaker.speaker.blog,
-      speaker.speaker.imageurl,
-      "blog",
-      "speakers"
-    );
-  });
-  return speakerRecommendations;
+  let speakerRecommendations = speakers
+    .filter(speaker => speakerLimit-- > 0)
+    .map(speaker => {
+      return new Recommendation(
+        speaker.speaker.name + "'s Blog",
+        speaker.speaker.blog,
+        speaker.speaker.imageurl,
+        "blog",
+        "speakers"
+      );
+    });
+
+  /*
+   Recommend based on talks
+   */
+  let talkRecommendations = [];
+  let youtubeRequests = speakers
+    .filter(speaker => speakerLimit-- > 0)
+    .map(speaker => {
+      // fire off request to YT api.
+    });
+
+  Promise.all(youtubeRequests).then(() => {});
+
+  return [].concat(speakerRecommendations).concat();
 }
