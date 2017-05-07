8dd96e367fd9e806e4f3010fa57ca97de4f3f138
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-05-07 17:42:46 +0100

    hide stats if unavailable

--- src/main/components/ReportStats.js
+++ src/main/components/ReportStats.js
@@ -40,49 +40,66 @@ const Orange = styled.span`
 
 class ReportStats extends Component {
   render() {
-    return (
-      <Row center="xs">
-        <Col xs={10}>
+    try {
+      throw new Error();
+      return (
+        <Row center="xs">
+          <Col xs={10}>
 
-          <Col xs={2}>
-            <Row start="xs">
-              <StatMainHeader>MyReport</StatMainHeader>
+            <Col xs={2}>
+              <Row start="xs">
+                <StatMainHeader>MyReport</StatMainHeader>
+              </Row>
+            </Col>
+
+            <Row center="xs">
+              <Col xs={10}>
+
+                <StatHeader>
+                  I spent <Orange>{this.props.minutes} minutes</Orange>
+                </StatHeader>
+
+                <StatValue>
+                  Attending <Orange>{this.props.talks} talks</Orange>
+                </StatValue>
+
+                <StatHeader>Learning what's new in...</StatHeader>
+
+                <StatListValue>
+                  <CommaList>
+                    {this.props.learning.map(track => (
+                      <CommaListItem key={track}>{track}</CommaListItem>
+                    ))}
+                  </CommaList>
+                </StatListValue>
+
+                <StatHeader>Networking with</StatHeader>
+
+                <StatValue>
+                  {" "}<Orange>{this.props.attendees}</Orange> Attendees
+                </StatValue>
+
+              </Col>
             </Row>
           </Col>
 
-          <Row center="xs">
-            <Col xs={10}>
+        </Row>
+      );
+    } catch (error) {
+      return (
+        <Row center="xs">
+          <Col xs={10}>
 
-              <StatHeader>
-                I spent <Orange>{this.props.minutes} minutes</Orange>
-              </StatHeader>
-
-              <StatValue>
-                Attending <Orange>{this.props.talks} talks</Orange>
-              </StatValue>
-
-              <StatHeader>Learning what's new in...</StatHeader>
-
-              <StatListValue>
-                <CommaList>
-                  {this.props.learning.map(track => (
-                    <CommaListItem key={track}>{track}</CommaListItem>
-                  ))}
-                </CommaList>
-              </StatListValue>
-
-              <StatHeader>Networking with</StatHeader>
-
-              <StatValue>
-                {" "}<Orange>{this.props.attendees}</Orange> Attendees
-              </StatValue>
-
+            <Col xs={2}>
+              <Row start="xs">
+                <StatMainHeader>MyReport</StatMainHeader>
+              </Row>
             </Col>
-          </Row>
-        </Col>
+          </Col>
 
-      </Row>
-    );
+        </Row>
+      );
+    }
   }
 }
 
