ca759c2f9b0cc09ecf3851e041b2d67296fab941
DanielLindsay <dl.12321.dl@googlemail.com>
2017-05-10 16:47:22 +0100

    created two tests that test the expectNotFoundOrRethrowError method

---
+++ src/test/unit/api/testingHelpers.test.js
@@ -0,0 +1,38 @@
+/**
+ * Created by DLINDSAY on 10-May-17.
+ */
+import {
+  expectNotFoundOrRethrowError,
+  raiseOrPassError
+} from "../../integration/api/testingHelpers";
+import {
+  UnexpectedErrorException,
+  UnexpectedSuccessException,
+  MappingSetupException
+} from "../../integration/api/testingErrors";
+
+test("Should rethrow the same error", () => {
+  let testError = new Error("error message");
+  testError.name = "MappingSetupException";
+
+  try {
+    expectNotFoundOrRethrowError(testError, "test message");
+    expect(false).toBe(true);
+  } catch (testError) {
+    expect(true).toBe(true);
+  }
+});
+
+test("Should throw an UnexpectedErrorException", () => {
+  let testError = new Error("error message");
+  testError.name = "NotInArrayException";
+
+  let testException = new UnexpectedErrorException("error message", testError);
+
+  try {
+    expectNotFoundOrRethrowError(testError, "test message");
+    expect(false).toBe(true);
+  } catch (testException) {
+    expect(true).toBe(true);
+  }
+});
