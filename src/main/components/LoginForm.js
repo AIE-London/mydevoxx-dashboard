be49cf1864220e12daffb950bded1b7e32a42a16
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-05-10 23:18:11 +0100

    refactored login form

--- src/main/components/LoginForm.js
+++ src/main/components/LoginForm.js
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
-
-import retrieveUuid from "../api/retrieveUuid";
-
 import { notify } from "react-notify-toast";
 import { Row } from "react-flexbox-grid";
 import styled from "styled-components";
 import { Redirect } from "react-router-dom";
 import * as firebase from "firebase";
 
+/*
+  Import components
+ */
 import Card from "./Card";
 
+/*
+  Import util functions
+ */
 import debugLog from "../utils/debugLog";
 
-let successURL = "https://personal.devoxx.co.uk";
+/*
+  Import API integration pieces
+ */
+import retrieveUuid from "../api/retrieveUuid";
 
+/*
+  Define Endpoints
+ */
+let successURL = "https://personal.devoxx.co.uk";
 const mockSuccessURL = "http://localhost:3000/report";
 
+if (["production", "integration"].indexOf(process.env.NODE_ENV) < 0) {
+  successURL = mockSuccessURL;
+}
+
+/*
+  Define Login Specific styled-components
+ */
 const LoginPage = styled(Row)`
   height: 100%;
   width: 100%;
@@ -40,13 +57,6 @@ const LoginCard = styled(Card)`
   margin-bottom: 4em;
 `;
 
-if (["production", "integration"].indexOf(process.env.NODE_ENV) < 0) {
-  successURL = mockSuccessURL;
-}
-// Initialize the FirebaseUI Widget using Firebase.
-
-// The start method will wait until the DOM is loaded.
-
 class LoginForm extends Component {
   uiConfig = {
     signInSuccessUrl: successURL,
@@ -57,7 +67,7 @@ class LoginForm extends Component {
       firebase.auth.GithubAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
-    tosUrl: "https://mydevoxx-dashboard.eu-gb.mybluemix.net/"
+    tosUrl: "https://personal-devoxx.eu-gb.mybluemix.net/login"
   };
 
   constructor(props) {
@@ -71,49 +81,64 @@ class LoginForm extends Component {
     this.goFetchUUID = this.goFetchUUID.bind(this);
     this.logIn = this.logIn.bind(this);
     this.registerListeners = this.registerListeners.bind(this);
+    // Register firebase state change listeners
     this.registerListeners();
   }
 
   logIn() {
+    // Start firebase in container provided;
     this.props.fbui.start("#firebaseui-auth-container", this.uiConfig);
   }
 
   goFetchUUID(email) {
+    // Set loading to true in state
     this.setState({ loading: true });
-    return retrieveUuid.getUUID(email).then(uuid => {
-      if (!uuid || uuid.length === 0) {
-        this.setState({ loading: false });
-        throw new Error("Missing UUID");
-      }
-      this.props.storeUUID(uuid).then(() => {
-        debugLog.log("UUID STORED");
-        this.props.onSignIn().then(() => {
-          debugLog.log("SIGNED IN -- REDIRECTING");
+    // Go get our UUID
+    return (
+      retrieveUuid
+        .getUUID(email)
+        // Then - take that UUID and check it's not null
+        .then(uuid => {
+          if (!uuid || uuid.length === 0) {
+            // It's a bad UUID - so throw an error
+            this.setState({ loading: false });
+            throw new Error("Missing UUID");
+          }
+          // We're ok - so just return the promise that we get
+          // from the function that will store the UUID
+          return this.props.storeUUID(uuid);
+        })
+        // Once we've stored the UUID - call the signin callback
+        .then(this.props.onSignIn)
+        // Once we've done that. Redirect to the home page.
+        .then(() => {
           this.setState({ redirect: true, loading: false });
-        });
-      });
-    });
+        })
+    );
   }
 
   registerListeners() {
+    // Call our function if the state of the user's authentication changes.
     this.props.firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        // User is signed in.
-
+        // User is signed in - grab their email
         let email = user.email;
-
+        // Go grab the users UUID with that email...
         this.goFetchUUID(email).catch(error => {
+          // We got an error - log it if in dev
           debugLog.log(error.message);
+          // Show notification to the user explaining the failed UUID request
           notify.show(
             "Login failed, please check you have a MyDevoxx account.",
             "error"
           );
         });
       } else {
+        // User signed out - notify them
         notify.show("Signed out");
-        // User is signed out.
       }
     }, function(error) {
+      // If Firebase login failed - notify user.
       notify.show("Login failed, please try again.", "error");
       debugLog.log(error);
     });
@@ -121,8 +146,10 @@ class LoginForm extends Component {
 
   componentDidMount() {
     try {
+      // Create firebase login
       this.logIn();
     } catch (error) {
+      // Failed creating firebase login - set state to reflect
       this.setState({ loginError: true });
     }
   }
